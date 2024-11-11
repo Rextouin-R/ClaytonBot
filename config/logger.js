09850c76d18bd124e6c925edf8a943873325e6e6
Rextouin-R <rattatouille1994@gmail.com>
2024-11-11 23:06:49 +0700

    Update logger.js

--- config/logger.js
+++ config/logger.js
@@ -1,1 +1,20 @@
 
+const winston = require("winston");
+const colors = require("./colors");
+
+const customFormat = winston.format.combine(
+  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+  winston.format.printf(({ timestamp, level, message }) => {
+    return `${colors.timestamp}${timestamp}${colors.reset} | ${
+      colors[level]
+    }${level.toUpperCase()}${colors.reset} | ${message}`;
+  })
+);
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || "info",
+  format: customFormat,
+  transports: [new winston.transports.Console()],
+});
+
+module.exports = logger;
